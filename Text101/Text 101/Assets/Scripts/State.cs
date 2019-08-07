8890ff592d8b286f069e7a7fa36a33dc3c7dd940
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2019-08-07 00:18:37 -0300

    Subundo fontes. Não lembro em que módulo está.

--- Text101/Text 101/Assets/Scripts/State.cs
+++ Text101/Text 101/Assets/Scripts/State.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [TextArea(14,10)] [SerializeField] string storyText;
+    [TextArea(14, 10)] [SerializeField] string storyText;
     [SerializeField] State[] nextStates;
 
     public string GetStoryText()
